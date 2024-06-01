069d9ca04dde92f276bce06b8baa40d759f53ac2
harshak01 <harshak03.62@gmail.com>
2024-06-01 21:16:13 +0530

    fixed build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,10 +6,11 @@ import {
   getAveragePrice,
   getEmailNotifType,
 } from "@/lib/utils";
-import { connectToDB } from "@/lib/mongoose";
+
 import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { connectToDB } from "@/lib/actions/mongoose";
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
